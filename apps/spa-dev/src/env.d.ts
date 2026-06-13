0860991867729d05d55a39e658794f7fca162c2b
Carlos R Moraes <carlos.moraes.as@gmail.com>
2026-06-13 06:05:53 -0300

    feat(spa-dev): display app version
    Refs #11

--- apps/spa-dev/src/env.d.ts
+++ apps/spa-dev/src/env.d.ts
@@ -1,6 +1,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
+    SPA_DEV_VERSION: string | undefined;
     VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
     VUE_ROUTER_BASE: string | undefined;
   }
---
+++ apps/spa-dev/src/services/app-version.ts
@@ -0,0 +1,1 @@
+export const APP_VERSION = process.env.SPA_DEV_VERSION ?? '0.0.0-dev';
